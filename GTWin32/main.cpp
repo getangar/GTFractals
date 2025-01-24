71132afe9320b0fb71db54d968962ed61898e59f
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2025-01-24 17:06:39 +0100

    Initial commit

---
+++ GTWin32/main.cpp
@@ -0,0 +1,77 @@
+#include<windows.h>
+
+void AddMenus(HWND);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	const LPCWSTR CLASS_NAME = L"GTWin32";
+
+	WNDCLASS wc = { 0 };
+
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	if (!RegisterClass(&wc)) {
+		MessageBox(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"GTWin32", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+
+	if (!hwnd) {
+		MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	AddMenus(hwnd);
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = { 0 };
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+};
+
+void AddMenus(HWND hwnd) {
+	HMENU hMenu = CreateMenu();
+	HMENU hFileMenu = CreateMenu();
+
+	AppendMenu(hFileMenu, MF_STRING, 1, L"Exit");
+	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+
+	SetMenu(hwnd, hMenu);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+		case WM_COMMAND:
+			if (LOWORD(wParam) == 1) {
+				PostQuitMessage(0);
+			}
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+		
+			const char* text = "Hello, World!";
+			TextOutA(hdc, 10, 10, text, strlen(text));
+
+			EndPaint(hwnd, &ps);
+		}
+
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
