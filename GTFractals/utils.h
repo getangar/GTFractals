4a802c6c417207db51d724bfdebebbe2af07d30c
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2025-02-03 15:39:43 +0100

    Resolving the issue with target platform, supporting now Intel 32/64bit and ARM 32/64bit

--- GTFractals/utils.h
+++ GTFractals/utils.h
@@ -1,10 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "platform.h"
+#include <windows.h>
 #include <stdio.h>
 #include <format>
 #include <debugapi.h>
 #include <cstdarg>
+
+
 #include "global.h"
 
 void DebugPrint(const char* format, ...);
