fdc56da74d2c4afa9465ebdafeac3eeaffe74ed6
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2025-02-05 09:24:47 +0100

    Adding PS script to update the build number at each build

---
+++ GTFractals/update_version.ps1
@@ -0,0 +1,18 @@
+$versionFile = "Version.rc"
+$versionPattern = '(?<=#define VERSION_BUILD )\d+'
+
+# Legge la versione corrente
+$versionContent = Get-Content $versionFile
+$buildNumber = [regex]::Match($versionContent, $versionPattern).Value
+
+# Incrementa il numero di build
+$newBuildNumber = [int]$buildNumber + 1
+$updatedContent = [regex]::Replace($versionContent, $versionPattern, $newBuildNumber)
+
+# Scrive la nuova versione nel file
+Set-Content -Path $versionFile -Value $updatedContent
+Write-Output "Updated build number to $newBuildNumber"
+
+# Esporta la versione per GitHub Actions
+$versionOutput = "0.1.$newBuildNumber"
+Write-Output "VERSION=$versionOutput" | Out-File -FilePath $env:GITHUB_ENV -Append
