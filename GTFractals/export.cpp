2ea1c96f0dbe2b3938922eb9e43f6ca58d05954f
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2025-02-12 14:18:47 +0100

    Fixing the issue with the saved image displaying toolbar and statusbar

--- GTFractals/export.cpp
+++ GTFractals/export.cpp
@@ -1,75 +1,95 @@
 #include "export.h"
 
 // Save the current fractal as a bitmap image
-BOOL SaveBitmap(HWND hwnd, LPCWSTR filename) {
+BOOL SaveBitmap(HWND hwnd, LPCWSTR filePath) {
+	// Get the client area of the window
+	RECT clientRect;
+	GetClientRect(hwnd, &clientRect);
+
+	// Adjust for UI elements (Toolbar & Status Bar)
+	int toolbarHeight = 0, statusBarHeight = 0;
+
+	// Get the height of the toolbar
+	if (hToolBar) {
+		RECT rcTool;
+		GetWindowRect(hToolBar, &rcTool);
+		toolbarHeight = rcTool.bottom - rcTool.top;
+	}
+
+	// Get the height of the status bar
+	if (hStatusBar) {
+		RECT rcStatus;
+		GetWindowRect(hStatusBar, &rcStatus);
+		statusBarHeight = rcStatus.bottom - rcStatus.top;
+	}
+
+	int fractalWidth = clientRect.right;
+	int fractalHeight = clientRect.bottom - toolbarHeight - statusBarHeight;
+
+	// Get the device context of the window
 	HDC hdcScreen = GetDC(hwnd);
 	HDC hdcMem = CreateCompatibleDC(hdcScreen);
+	HBITMAP hbmMem = CreateCompatibleBitmap(hdcScreen, fractalWidth, fractalHeight);
+	SelectObject(hdcMem, hbmMem);
 
-	RECT rect;
-	GetClientRect(hwnd, &rect);
-	int width = rect.right;
-	int height = rect.bottom;
+	// Copy only the fractal drawing area
+	BitBlt(hdcMem, 0, 0, fractalWidth, fractalHeight, hdcScreen, 0, toolbarHeight, SRCCOPY);
 
-	HBITMAP hbmScreen = CreateCompatibleBitmap(hdcScreen, width, height);
-	SelectObject(hdcMem, hbmScreen);
-
-	BitBlt(hdcMem, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
-
-	BITMAP bmp;
-	GetObject(hbmScreen, sizeof(BITMAP), &bmp);
-
+	// Save the bitmap to file
 	BITMAPFILEHEADER bmfHeader;
 	BITMAPINFOHEADER bi;
 
 	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = bmp.bmWidth;
-	bi.biHeight = -bmp.bmHeight; // Negative to avoid vertical flipping
+	bi.biWidth = fractalWidth;
+	bi.biHeight = -fractalHeight; // Negative height for top-down bitmap
 	bi.biPlanes = 1;
-	bi.biBitCount = 24; // 24-bit color
+	bi.biBitCount = 32;
 	bi.biCompression = BI_RGB;
-	bi.biSizeImage = ((bmp.bmWidth * 3 + 3) & ~3) * bmp.bmHeight;
+	bi.biSizeImage = 0;
 	bi.biXPelsPerMeter = 0;
 	bi.biYPelsPerMeter = 0;
 	bi.biClrUsed = 0;
 	bi.biClrImportant = 0;
 
-	DWORD dwBmpSize = bi.biSizeImage;
+	DWORD dwBmpSize = ((fractalWidth * bi.biBitCount + 31) / 32) * 4 * fractalHeight;
 	HANDLE hDIB = GlobalAlloc(GHND, dwBmpSize);
 	char* lpbitmap = (char*)GlobalLock(hDIB);
 
-	GetDIBits(hdcScreen, hbmScreen, 0, (UINT)bmp.bmHeight, lpbitmap, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+	GetDIBits(hdcScreen, hbmMem, 0, fractalHeight, lpbitmap, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
 
-	HANDLE hFile = CreateFile(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE hFile = CreateFile(filePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hFile == INVALID_HANDLE_VALUE) {
+		MessageBox(hwnd, L"Failed to create file!", L"Error", MB_ICONERROR);
 		GlobalUnlock(hDIB);
 		GlobalFree(hDIB);
+		DeleteObject(hbmMem);
 		DeleteDC(hdcMem);
 		ReleaseDC(hwnd, hdcScreen);
-		DeleteObject(hbmScreen);
-		return FALSE;
+		return false;
 	}
 
 	DWORD dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-	bmfHeader.bfType = 0x4D42;
+	bmfHeader.bfType = 0x4D42; // BM
 	bmfHeader.bfSize = dwSizeofDIB;
 	bmfHeader.bfReserved1 = 0;
 	bmfHeader.bfReserved2 = 0;
 	bmfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
 
-	DWORD dwWritten;
-	WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
-	WriteFile(hFile, (LPSTR)&bi, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
-	WriteFile(hFile, (LPSTR)lpbitmap, dwBmpSize, &dwWritten, NULL);
+	DWORD dwBytesWritten;
+	WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)&bi, sizeof(BITMAPINFOHEADER), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)lpbitmap, dwBmpSize, &dwBytesWritten, NULL);
 
 	CloseHandle(hFile);
 	GlobalUnlock(hDIB);
 	GlobalFree(hDIB);
+	DeleteObject(hbmMem);
 	DeleteDC(hdcMem);
 	ReleaseDC(hwnd, hdcScreen);
-	DeleteObject(hbmScreen);
 
-	return TRUE;
+	return true;
 }
+
 
 // Print the current fractal
 BOOL PrintFractal(HWND hwnd) {
